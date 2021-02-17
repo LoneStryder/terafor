a6f7f24e1b45685449abdedab0c18b5a50319b06
Stryder <strygant@gmail.com>
2021-02-17 22:55:33 +0000

    Add silver armor
    • Added silver helmet, chestplate, leggings and boots to the ModItems registry.
    • Added a model, localized name and crafting recipe for each armor piece.
    • Introduced ModArmorMaterials enum for custom armor materials. (contains only silver for now)

--- src/main/java/stryder/terafor/item/ModItems.java
+++ src/main/java/stryder/terafor/item/ModItems.java
@@ -1,5 +1,6 @@
 package stryder.terafor.item;
 
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -26,4 +27,8 @@ public class ModItems {
 	public static final RegistryObject<Item> SILVER_SHOVEL = ITEMS.register("silver_shovel", () -> new ShovelItem(ModItemTiers.SILVER, 1.5F, -3.0F, createProperties()));
 	public static final RegistryObject<Item> SILVER_AXE = ITEMS.register("silver_axe", () -> new AxeItem(ModItemTiers.SILVER, 6.0F, -3.1F, createProperties()));
 	public static final RegistryObject<Item> SILVER_HOE = ITEMS.register("silver_hoe", () -> new HoeItem(ModItemTiers.SILVER, -1, -2.0F, createProperties()));
+	public static final RegistryObject<Item> SILVER_HELMET = ITEMS.register("silver_helmet", () -> new ArmorItem(ModArmorMaterials.SILVER, EquipmentSlotType.HEAD, createProperties()));
+	public static final RegistryObject<Item> SILVER_CHESTPLATE = ITEMS.register("silver_chestplate", () -> new ArmorItem(ModArmorMaterials.SILVER, EquipmentSlotType.CHEST, createProperties()));
+	public static final RegistryObject<Item> SILVER_LEGGINGS = ITEMS.register("silver_leggings", () -> new ArmorItem(ModArmorMaterials.SILVER, EquipmentSlotType.LEGS, createProperties()));
+	public static final RegistryObject<Item> SILVER_BOOTS = ITEMS.register("silver_boots", () -> new ArmorItem(ModArmorMaterials.SILVER, EquipmentSlotType.FEET, createProperties()));
 }
